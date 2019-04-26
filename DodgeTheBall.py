1449c3cd702b926d41640f2fc8c9dc1f21f362da
JavierJRRamirez <Javier.Ramirez@colorado.edu>
2019-04-25 20:02:56 -0600

    Update DodgeTheBall.py

--- DodgeTheBall.py
+++ DodgeTheBall.py
@@ -99,7 +99,6 @@ class Alien(Actor):
         #self.facing = random.choice((-1,1)) * ALIEN_SPEED
         
         self.rect.right = random.choice(range(SCREENRECT.left, SCREENRECT.right, 10))
-<<<<<<< HEAD
     
     def update(self):
         global SCREENRECT
@@ -108,7 +107,6 @@ class Alien(Actor):
 #     self.facing = -self.facing;
 #     self.rect.top = self.rect.bottom + 3
 #     self.rect = self.rect.clamp(SCREENRECT)
-=======
             
     def update(self):
         global SCREENRECT
@@ -117,7 +115,6 @@ class Alien(Actor):
         #     self.facing = -self.facing;
         #     self.rect.top = self.rect.bottom + 3
         #     self.rect = self.rect.clamp(SCREENRECT)
->>>>>>> bdaba2df3fb1769b7162dffacdf78f2d86120e77
 
 
 class Explosion(Actor):
@@ -152,13 +149,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREENRECT.size, 0)
     clock = pygame.time.Clock()
-<<<<<<< HEAD
-    
-    
-=======
 
-
->>>>>>> bdaba2df3fb1769b7162dffacdf78f2d86120e77
     # Load the Resources
     Img.background = load_image('background.gif', 0)
     Img.shot = load_image('shot.gif', 1)
@@ -180,15 +171,7 @@ def main():
     aliens = [Alien()]
     shots = []
     explosions = []
-<<<<<<< HEAD
-    
-    
-    
-=======
 
-
-
->>>>>>> bdaba2df3fb1769b7162dffacdf78f2d86120e77
     # Main loop
     while player.alive or explosions:
         clock.tick(FRAMES_PER_SEC)
@@ -216,7 +199,6 @@ def main():
         mousex, mousey = pygame.mouse.get_pos()
         direction = 0
         if mousex < 341:
-<<<<<<< HEAD
             direction = -1
         elif mousex > 682:
             direction = 1
@@ -226,9 +208,6 @@ def main():
 
         # direction = keystate[K_RIGHT] - keystate[K_LEFT]
         player.move(direction)
-        
-        
-=======
         	direction = -1
         elif mousex > 682:
         	direction = 1
@@ -238,9 +217,7 @@ def main():
 
         # direction = keystate[K_RIGHT] - keystate[K_LEFT]
         player.move(direction)
-
-
->>>>>>> bdaba2df3fb1769b7162dffacdf78f2d86120e77
+        
         # Create new shots
         if not player.reloading and keystate[K_SPACE] and len(shots) < MAX_SHOTS:
             shots.append(Shot(player))
@@ -274,13 +251,6 @@ def main():
         # Draw everybody
         for actor in [player] + aliens + shots + explosions:
             actor.draw(screen)
-<<<<<<< HEAD
-        
-        
-=======
-
-
->>>>>>> bdaba2df3fb1769b7162dffacdf78f2d86120e77
         pygame.display.update(dirtyrects)
         dirtyrects = []
 
